8b855d044ca661cbc7a53d56cc43456422112571
natli-think <lintaoommen2154@gmail.com>
2024-09-08 15:48:59 +0300

    Improve UI and upload to Dropbox

--- src/main/java/com/geoSnap/imageuploader/controller/ImageController.java
+++ src/main/java/com/geoSnap/imageuploader/controller/ImageController.java
@@ -72,4 +72,9 @@ public class ImageController {
         return "view";
     }
 
+    @GetMapping("/geoSnap")
+    public String returnInfo(){
+        return "about";
+    }
+
 }
--- src/main/java/com/geoSnap/imageuploader/service/imageService.java
+++ src/main/java/com/geoSnap/imageuploader/service/imageService.java
@@ -1,13 +1,17 @@
 package com.geoSnap.imageuploader.service;
 
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.WriteMode;
 import com.geoSnap.imageuploader.model.Image;
 import com.geoSnap.imageuploader.repository.imageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Service
@@ -17,21 +21,44 @@ public class imageService {
     private imageRepository imageRepository;
 
     public Image saveImage(MultipartFile file, Float latitude, Float longitude) throws IOException {
-        // Get the project root directory
-        String projectDir = System.getProperty("user.dir");
+        final String ACCESS_TOKEN = "sl.B8h3CMbSjj7ribhb2UdvCiV8hivA7cSuOz95kEWTnZBX8Nrf5TFdpq_q3JC725OgyJL2J9m9loGVMG4GRSyZEGD7PG9Nd_HUJQezeoyKPXlClvWuApXGiY7cIVDxmMEuwhER-o8abkfC-fUibADQlNM";
 
-        // Define the path to save the file
-        File directory = new File(projectDir, "imageuploader/src/main/resources/static/images");
+//        // Get the project root directory
+//        String projectDir = System.getProperty("user.dir");
+//
+//        // Define the path to save the file
+//        File directory = new File(projectDir, "imageuploader/src/main/resources/static/images");
+//
+//        // Ensure directory exists
+//        if (!directory.exists()) {
+//            directory.mkdirs();
+//        }
+//
+//        // Define the file path
+//        String fileName = file.getOriginalFilename();
+//        File uploadFile = new File(directory, fileName);
+//        file.transferTo(uploadFile);
 
-        // Ensure directory exists
-        if (!directory.exists()) {
-            directory.mkdirs();
+        // Initialize Dropbox client
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+
+        // Define the Dropbox path
+        String dropboxPath = "/geoSnap/" + file.getOriginalFilename();
+
+        String uploadFile="";
+        try (InputStream in = file.getInputStream()) {
+            // Upload to Dropbox
+            FileMetadata metadata = client.files().uploadBuilder(dropboxPath)
+                    .withMode(WriteMode.OVERWRITE)
+                    .uploadAndFinish(in);
+
+            // Generate a shared link for the file
+            uploadFile = client.sharing().createSharedLinkWithSettings(metadata.getPathLower()).getUrl();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IOException("Error uploading file to Dropbox", e);
         }
-
-        // Define the file path
-        String fileName = file.getOriginalFilename();
-        File uploadFile = new File(directory, fileName);
-        file.transferTo(uploadFile);
 
         // Create and save image metadata
         Image image = new Image();
@@ -39,7 +66,7 @@ public class imageService {
         image.setSize(file.getSize());
         image.setLatitude(latitude);
         image.setLongitude(longitude);
-        image.setFilePath(uploadFile.getAbsolutePath());
+        image.setFilePath(uploadFile);
 
         return imageRepository.save(image);
     }
